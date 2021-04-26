a2de44297582d3c4c52911e3a104cea906022774
Nick Overd <nickoverd2014@outlook.com>
2021-04-26 15:27:04 +0100

    advanced enemies added

--- src/Enemy.java
+++ src/Enemy.java
@@ -12,10 +12,7 @@ public class Enemy {
 		this.health=health;
 		this.speed=speed;
 		this.name=name;
-//		Enemy.add(Enemy_builder(20,5,10,"goblin"));
-//		Enemy.add(Enemy_builder(50,10,1,"orge"));
-//		Enemy.add(Enemy_builder(15,10,5,"skeleton"));
-//		
+	
 	}
 	
 }
