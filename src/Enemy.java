f30cf97cbba5b5f6da05a954d79e31b9e5a5b6fa
Nick Overd <nickoverd2014@outlook.com>
2021-04-26 15:25:22 +0100

    advanced enemies added

---
+++ src/Enemy.java
@@ -0,0 +1,21 @@
+
+public class Enemy {
+	
+	int health;
+	int dam;
+	int speed;
+	String name;
+	
+	
+	public Enemy(int health,int dam,int speed,String name) {
+		this.dam=dam;
+		this.health=health;
+		this.speed=speed;
+		this.name=name;
+//		Enemy.add(Enemy_builder(20,5,10,"goblin"));
+//		Enemy.add(Enemy_builder(50,10,1,"orge"));
+//		Enemy.add(Enemy_builder(15,10,5,"skeleton"));
+//		
+	}
+	
+}
