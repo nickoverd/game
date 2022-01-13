ae17366d8891be5559cae45247569b6fc6c1e01e
nickoverd <nickoverd2014@outlook.com>
2022-01-13 16:08:26 +0000

    Update treasure.java

--- src/treasure.java
+++ src/treasure.java
@@ -4,9 +4,8 @@ public class treasure {
 	String[] all_loot = {"Lanturn","Sword","Spy glass","Potion"};
 	// lanturn will provide light in areas - sword fight - spy glass -
 	public String item() {
-		int numb = rand.nextInt(4);
+		int numb = rand.nextInt(3);
 		String items = all_loot[numb];
-		
 		System.out.println("you have recieved a " + all_loot[numb]);
 		return items;
 	}
